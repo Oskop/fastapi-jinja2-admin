ea4b3c6db642ce555a00bd42bd9b1d198915519b
kulitek <oskop17@gmail.com>
2021-08-10 13:20:00 +0700

    change to admin panel

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/assets", StaticFiles(directory="templates/assets"), name="assets")
+app.mount("/assets", StaticFiles(directory="view/static/assets"), name="assets")
+# app.mount("/admin_assets", StaticFiles(directory="view/admin/assets"), name="admin_assets")
 
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="view")
 
 
 @app.get("/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
-@app.get("/kardinah", response_class=HTMLResponse)
+@app.get("/kardinah/pendaftaran", response_class=HTMLResponse)
 async def read_item(request: Request):
-    return templates.TemplateResponse("main/home.html", {"request": request})
+    return templates.TemplateResponse("templates/index.html", {"request": request})
 
 @app.get("/kardinah/admin", response_class=HTMLResponse)
-async def admin_home(arg):
-    return templates.TemplateResponse("admin/main/index.html")
+async def admin_home(request: Request):
+    return templates.TemplateResponse("admin/base/index.html", {"request": request})
